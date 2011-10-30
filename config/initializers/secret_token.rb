e2e23e700db58330054b18accb2dc8d63039ae28
kende <kende.j@gmail.com>
2011-10-30 17:52:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitacademy::Application.config.secret_token = '7c6b57e1a0475477506539b0c8824cb6e017550968a792aed165816aaaafe620d3b9b5e08c9c0c6402db421671d98e3cf48acab48fb0ef930c2f2b8d71070dfc'
